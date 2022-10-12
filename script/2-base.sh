30accdcb9ee8fce9927b8444a72cda3d4889f667
Hi120ki <12624257+hi120ki@users.noreply.github.com>
2022-10-12 12:53:54 +0900

    gtp5g v0.6.6
    Signed-off-by: Hi120ki <12624257+hi120ki@users.noreply.github.com>

--- script/2-base.sh
+++ script/2-base.sh
@@ -7,6 +7,6 @@ sudo apt update
 sudo apt install -y unzip make build-essential
 
 echo "[i] install gtp5g"
-cd ~ ; wget -q https://github.com/free5gc/gtp5g/archive/refs/tags/v0.6.1.zip ; unzip v0.6.1.zip ; cd gtp5g-0.6.1 ; make ; sudo make install
+cd ~ ; wget -q https://github.com/free5gc/gtp5g/archive/refs/tags/v0.6.6.zip ; unzip v0.6.6.zip ; cd gtp5g-0.6.6 ; make ; sudo make install
 cat /etc/modules | grep gtp5g
 lsmod | grep gtp5g
